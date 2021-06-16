f9905d5fd12c34c9e7bcdc0ae210a0a244184389
akbarwck55 <85973302+akbarwck55@users.noreply.github.com>
2021-06-16 09:18:36 +0800

    Add files via upload

---
+++ locker.cpp
@@ -0,0 +1,33 @@
+#include "locker.h"
+
+void add_data(locker &1, int x) {
+
+ if (locker.num != locker.max-1) {
+ storage.data[storage.num] = x;
+ storage.num = storage.num +1;
+ } else {
+ cout<<"full"<<endl;
+ }
+}
+
+void view_data(locker 1) {
+int = 1
+
+while (i<1.num && i<max) {
+    i++;
+    cout<<i.data[i];
+
+}
+cout<<endl;
+
+}
+
+}
+
+
+void search_data(locker 1, int x) {
+
+
+
+}
+
---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "locker.h"
+
+int main() {
+
+add_data(storage, 6);
+view_data(storage);
+
+add_data(storage, 8);
+view_data(storage);
+
+add_data(storage, 4);
+view_data(storage);
+
+add_data(storage, 2);
+view_data(storage);
+
+}
+
+
